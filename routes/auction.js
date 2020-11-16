4cbbb910040b98ff0a9f40fdb4db4e17c09ec945
Ricardo Andre <ricardoandre.dasein@gmail.com>
2020-11-16 19:59:53 +0000

    mais styles... isto demora p crl..

--- routes/auction.js
+++ routes/auction.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router  = express.Router();
 
-/* GET home page */
+/* GET home page 
 router.get('/Auction/auction', (req, res, next) => {
   res.render('Auction/auction');
 });
+
+router.get('/Auction/my-auction', (req, res, next => {
+  res.render('Auction/my-auction')
+}))*/
 
 module.exports = router;
